013419c7a10da9c2b67f9592891f8d1cfd0c5017
Rhys Evans <rhys.evans@redditchconnected.co.uk>
2021-11-03 10:02:02 +0000

    composer init for package

---
+++ src/DeploymentActionsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RC\DeploymentActions;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeploymentActionsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
